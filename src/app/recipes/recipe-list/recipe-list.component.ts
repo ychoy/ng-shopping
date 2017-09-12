517ed51aec80a94a8ad9f301f1e836538ed968aa
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-09-12 06:10:39 -0700

    import recipes into Recipe List component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Tiramisu', 'Delectable. Use only the best ingredients!', 'https://i.imgur.com/3IHUG9t.jpg'),
+    new Recipe('Tiramisu', 'Delectable. Use only the best ingredients!', 'https://i.imgur.com/3IHUG9t.jpg'),
+    new Recipe('Tiramisu', 'Delectable. Use only the best ingredients!', 'https://i.imgur.com/3IHUG9t.jpg')
+  
+
+  ];
 
   constructor() { }
 
